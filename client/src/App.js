754845b890fe779374a5320a4c686bf9da582232
Vladislav <Chubar.Vladislav@kronas.com.ua>
2023-04-05 17:21:21 +0300

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenens/homePage";
+import LoginPage from "scenens/loginPage";
+import ProfilePage from "scenens/profilePage";
 
 const App = () => {
-  return <div className="app">hello world</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
