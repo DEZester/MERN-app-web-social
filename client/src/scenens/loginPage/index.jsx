c85b586b96c342c0bda971643aa382fe1756518a
Vladislav <Chubar.Vladislav@kronas.com.ua>
2023-04-12 17:38:48 +0300

    Login page

--- client/src/scenens/loginPage/index.jsx
+++ client/src/scenens/loginPage/index.jsx
@@ -1,5 +1,37 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
-  return <div></div>;
+  const theme = useTheme();
+  const isNonMobileScreeens = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontSize="32px" fontWeight="bold" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreeens ? "50%" : "93%"}
+        p={"2rem"}
+        m={"2rem auto"}
+        borderRadius={"1.5rem"}
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight={"500"} variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to the Socipedia, the Social Meadia for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
