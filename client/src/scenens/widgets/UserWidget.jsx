df7af63f6928c9f29c2775dcfe65d51d8ad451f2
Vladislav <Chubar.Vladislav@kronas.com.ua>
2023-04-14 09:24:34 +0300

    Fix error

--- client/src/scenens/widgets/UserWidget.jsx
+++ client/src/scenens/widgets/UserWidget.jsx
@@ -22,17 +22,17 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const reponse = await fetch(`http://localhost:3001/users/${userId}`, {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
-    const data = await reponse.json();
+    const data = await response.json();
     setUser(data);
   };
 
   useEffect(() => {
     getUser();
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (!user) {
     return null;
