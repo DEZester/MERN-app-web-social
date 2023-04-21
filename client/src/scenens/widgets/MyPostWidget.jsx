48c22035ae983a14780ad56f6c2a415f4a68fd40
Vladislav <Chubar.Vladislav@kronas.com.ua>
2023-04-21 13:57:38 +0300

    Fix Bugs

--- client/src/scenens/widgets/MyPostWidget.jsx
+++ client/src/scenens/widgets/MyPostWidget.jsx
@@ -45,13 +45,14 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picture", image);
       formData.append("picturePath", image.name);
     }
+
     const response = await fetch(`http://localhost:3001/posts`, {
       method: "POST",
-      headers: { Athorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` },
       body: formData,
     });
     const posts = await response.json();
-    dispatch(setPost({ posts }));
+    dispatch(setPosts({ posts }));
     setImage(null);
     setPost("");
   };
