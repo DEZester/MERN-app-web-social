3b1d943297a88e88465cd043b6e6ccbd81ff08e2
Vladislav <Chubar.Vladislav@kronas.com.ua>
2023-04-21 16:48:37 +0300

    Posts

--- client/src/scenens/homePage/index.jsx
+++ client/src/scenens/homePage/index.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Navbar from "scenens/navbar";
 import UserWidget from "scenens/widgets/UserWidget";
 import MyPostWidget from "scenens/widgets/MyPostWidget";
+import PostsWidget from "scenens/widgets/PostsWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -26,6 +27,7 @@ const HomePage = () => {
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
         </Box>
         {isNonMobileScreens && <Box flexBasis={"26%"}></Box>}
       </Box>
